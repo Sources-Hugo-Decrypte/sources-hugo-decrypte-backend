9218040cc6d445636742426dc857488da1a83631
git.youn <git.youn@gmail.com>
2022-01-17 10:49:46 +0100

    new log file for error only

--- src/main.py
+++ src/main.py
@@ -1,17 +1,18 @@
 
-from datetime import datetime
+from time import time
+from datetime import datetime, timedelta
 from dbUpdater import *
 
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
-    print(ROOT_PATH)
     if 1:
         if not os.path.exists(LOG_PATH): os.mkdir(LOG_PATH)
         creationDate = datetime.now()
         fileLog = LOG_PATH + os.sep + creationDate.strftime("%Y%m%d_%H%M%S") + '.log'
+        fileLogError = LOG_PATH + os.sep + creationDate.strftime("%Y%m%d_%H%M%S") + '_error.log'
         myLogger = logging.getLogger(__name__)
-        for file in [fileLog]:
+        for file in [fileLog, fileLogError]:
             with open(file, 'w') as f:
                 f.write(LOG_HEADER % creationDate.strftime("%Y %m %d - %H:%M:%S"))
                 f.close()
@@ -21,9 +22,26 @@ if __name__ == '__main__':
         f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(funcName)s - %(message)s')
         f_handler.setFormatter(f_format)
         myLogger.addHandler(f_handler)
+        # only error log file :
+        fe_handler = logging.FileHandler(fileLogError)
+        fe_handler.setLevel(logging.ERROR)
+        fe_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(funcName)s - %(message)s')
+        fe_handler.setFormatter(fe_format)
+        myLogger.addHandler(fe_handler)
 
     updater = DatabaseUpdater(logger=myLogger, database=DB_NAME, user=DB_USER, password=DB_PWD, host=DB_HOST)
-    if 1:
+    if 0:
         updater.db.clearDb()
         updater.createTablesIntoDb()
-        updater.dailyUpdate()
+    if 1:
+        try:
+            timeStart = time()
+            ### put below this line the tasks to execute ###
+            updater.dailyUpdate()
+            ### end of tasks to execute ###
+            timeEnd = time()
+            timeElapsed = timedelta(seconds=timeEnd-timeStart)
+            myLogger.info(f"Program Done. Elapsed time {timeElapsed}")
+        except Exception:
+            myLogger.critical("Major Exception Occurred", exc_info=True)
+
