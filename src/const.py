a5b9cf3e876fb6ac7ec969558ddf0bf6431ced94
git.youn <git.youn@gmail.com>
2022-01-25 18:09:29 +0100

    comments

--- src/const.py
+++ src/const.py
@@ -48,10 +48,11 @@ DB_DEFAULT_VALUE = None
 ## # # # #     Video details table     # # # # #
 ##
 ## Structure :
-##  - VIDEO_ID      : [String] unique video ID given by youtube
-##  - VIDEO_NAME    : [String] name of the youtube video (video title)
-##  - VIDEO_DATE    : [Timestamp] release date of the video
-##  - VIDEO_DESC    : [String] description associated to the video
+##  - video_id      : [String] unique video ID given by youtube
+##  - video_name    : [String] name of the youtube video (video title)
+##  - video_date    : [Timestamp] release date of the video
+##  - video_img     : [String] link to the video's picture
+##  - video_desc    : [String] description associated to the video
 ##
 class VIDEO_TABLE:
     NAME = "video_table"
@@ -59,8 +60,8 @@ class VIDEO_TABLE:
     COL_ID = "video_id"
     COL_NAME = "video_name"
     COL_DATE = "video_date"
-    COL_DESC = "video_desc"
     COL_IMG  = "video_img"
+    COL_DESC = "video_desc"
     # variables :
     dicStructure = {COL_ID: "text",
                     COL_NAME: "text",
@@ -73,17 +74,18 @@ class VIDEO_TABLE:
 ## # # # #     Urls table     # # # # #
 ##
 ## Structure :
-##  - URL_FULL      : [String] full url
-##  - URL_SHORT     : [String] first part of the full_url
-##  - URL_VIDEO_ID  : [String] youtube video ID where this url was used
-##  - URL_CHECK_MSG : [String] message returned when url has been checked
+##  - url_full          : [String] full url
+##  - url_short         : [String] first part of the full_url
+##  - url_video_id      : [String] youtube video ID where this url was used
+##  - url_check_status  : [String] status returned by the url checking analysis
+##  - url_check_msg     : [String] detailed message returned by the url checking analysis
 ##
 class URL_TABLE:
     NAME = "url_table"
     # Columns :
-    COL_URL_SHORT = "url_short"
-    COL_URL_FULL = "url_full"
     COL_VIDEO_ID = "url_video_id"
+    COL_URL_FULL = "url_full"
+    COL_URL_SHORT = "url_short"
     COL_CHECK_STATUS = "url_check_status"
     COL_CHECK_MSG = "url_check_msg"
     # Variables :
@@ -98,8 +100,8 @@ class URL_TABLE:
 ## # # # #     Register table     # # # # #
 ##
 ## Structure :
-##  - REGISTER_SHORT_URL    : [String] short url of the page (ex : 'www.ouest-france.fr' or 'actu.fr')
-##  - REGISTER_NAME         : [String] common name of the media (ex : 'Ouest-France' or 'Actu')
+##  - register_url_short        : [String] short url of the page (ex : 'www.ouest-france.fr' or 'www.actu.fr')
+##  - register_common_name      : [String] common name of the media (In reality, we used the domain names. ex : 'ouest-france.fr' or 'actu.fr')
 ##
 class REGISTER_TABLE:
     NAME = "register_table"
@@ -115,9 +117,9 @@ class REGISTER_TABLE:
 ## # # # #     Youtube links table     # # # # #
 ##
 ## Structure :
-##  - LINKS_YOUTUBE_URL         : [String] full youtube url
-##  - LINKS_YOUTUBE_CHANNEL     : [String] if it is a video link, name of the author channel
-##  - LINKS_YOUTUBE_MSG         : [String] message of the link analysis
+##  - links_ytb_url         : [String] full youtube url
+##  - links_ytb_channel     : [String] if it is a video link, name of the author channel
+##  - links_ytb_msg         : [String] message of the link analysis
 ##
 class LINKS_YTB_TABLE:
     NAME = "links_ytb_table"
@@ -135,8 +137,8 @@ class LINKS_YTB_TABLE:
 ## # # # #     Blacklist youtube channels table     # # # # #
 ##
 ## Structure :
-##  - BLACKLIST_YTB_CHANNEL_NAME        : [String] name of the channel blacklisted
-##  - BLACKLIST_YTB_CHANNEL_REASON      : [String] reason why it is blacklisted (mandatory)
+##  - blacklist_ytb_channel_name        : [String] name of the channel blacklisted
+##  - blacklist_ytb_channel_reason      : [String] reason why it is blacklisted (mandatory)
 ##
 class BLACKLIST_YTB_CHANNEL_TABLE:
     NAME = "blacklist_ytb_channel_table"
@@ -152,8 +154,8 @@ class BLACKLIST_YTB_CHANNEL_TABLE:
 ## # # # #     Blacklist table     # # # # #
 ##
 ## Structure :
-##  - BLACKLIST_URL         : [String] short or full blacklisted url
-##  - BLACKLIST_REASON      : [String] reason why it is blacklisted (mandatory)
+##  - blacklist_url         : [String] short or full blacklisted url
+##  - blacklist_reason      : [String] reason why it is blacklisted (mandatory)
 ##
 class BLACKLIST_TABLE:
     NAME = "blacklist_table"
@@ -165,7 +167,6 @@ class BLACKLIST_TABLE:
                     COL_REASON: "text"}
     listKeys = [COL_URL]
     listColumns = list(dicStructure.keys())
-
 
 # List of all XXX_TABLE objects :
 DB_LIST_TABLES_OBJ = [VIDEO_TABLE,
@@ -183,7 +184,8 @@ DB_LIST_TABLES_OBJ = [VIDEO_TABLE,
 
 # Structure is as follows : ((url, reason_of_blacklisting), (url, reason_of_blacklisting), ...)
 LIST_BLACKLISTED_URL = (("radio-londres.fr", "This is and old domain name, owned by Hugo Travers, for one of the projects he made. It now redirects to his youtube home page account"),
-                        ("https://open.spotify.com/show/6y1PloEyNsCNJH9vHias4T?si=pz8U9CGkTCO_IGSEnMVxVw", "Hugo Decrypte's podcasts home page"))
+                        ("https://open.spotify.com/show/6y1PloEyNsCNJH9vHias4T?si=pz8U9CGkTCO_IGSEnMVxVw", "Hugo Decrypte's podcasts home page"),
+                        ("https://drive.google.com/folderview?id=0B7ef7p49PPuUTVVVU0JsMWRVY3c&usp=sharing", "Google drive document shared by Hugo Decrypte"))
 
 # Structure is as follows : ((channel_name, reason_of_blacklisting), (channel_name, reason_of_blacklisting), ...)
 LIST_BLACKLISTED_YTB_CHANNEL = (("HugoDécrypte - Actus du jour", "Hugo Decrypte channel"),
